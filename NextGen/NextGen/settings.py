02b9f37b163c9ec3d7822c4c874d2c936934347f
Snake11235 <34827459+Snake11235@users.noreply.github.com>
2021-09-28 15:24:15 +0545

    req

--- NextGen/NextGen/settings.py
+++ NextGen/NextGen/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5mt+oxz@91apx^w#dfssldg$-_9=-)yzv20x3!tv4m9+^m$6f4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
